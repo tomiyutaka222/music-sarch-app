1e2cd4e7c3a314da671d3c623c5f180fcf3e638b
tomiyutaka222 <68626716+tomiyutaka222@users.noreply.github.com>
2020-07-26 14:46:50 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app color="pink" dark>
+      <v-toolbar-title class="headline text-uppercase">
+        <router-link to="/" style="color:white;text-decoration:none;"><span>MUSIC SEARCH</span></router-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        to="/"
+      >
+        HOME
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
